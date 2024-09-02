f50a41abb8f331ea15b0a909de7a846cdeda6ed5
sanketares <sanketares@gmail.com>
2024-09-02 02:07:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
 }
 
 
-# Create a VPC
-resource "aws_vpc" "my_vpc-2" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "my-vpc-2"
-  }
-}
 
 resource "aws_instance" "my_instance" {
   ami           = "ami-066784287e358dad1" # Example AMI ID, change as necessary

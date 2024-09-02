b48aff0b291515e8f0a191601e9cadada4ebcd56
sanketares <sanketares@gmail.com>
2024-09-02 02:48:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,33 @@ provider "aws" {
 }
 
 
+data "aws_ami" "redhat" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9*HVM-2024*-x86_64*GP3"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-066784287e358dad1" # Example AMI ID, change as necessary
+  ami           = data.aws_ami.redhat.id
   instance_type = "t2.micro"               # Instance type
 
   tags = {
